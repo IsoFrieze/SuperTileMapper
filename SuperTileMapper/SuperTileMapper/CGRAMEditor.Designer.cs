8b4955bd3da819e1aed8441c76a352f9b3af9764
Alex Losego <dotsarecool1@gmail.com>
2018-08-20 12:15:22 -0400

    CGRAM details
    + details panel
    + can change the color by clicking on the box
    - other controls will work eventually
    - panel for hex editor implemented

--- SuperTileMapper/SuperTileMapper/CGRAMEditor.Designer.cs
+++ SuperTileMapper/SuperTileMapper/CGRAMEditor.Designer.cs
@@ -36,8 +36,27 @@
             this.hexEditorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.colorPropertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -47,7 +66,7 @@
             this.viewToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(256, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(656, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -87,17 +106,17 @@
             // 
             // hexEditorToolStripMenuItem
             // 
-            this.hexEditorToolStripMenuItem.Enabled = false;
             this.hexEditorToolStripMenuItem.Name = "hexEditorToolStripMenuItem";
             this.hexEditorToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
             this.hexEditorToolStripMenuItem.Text = "Hex Editor";
+            this.hexEditorToolStripMenuItem.Click += new System.EventHandler(this.hexEditorToolStripMenuItem_Click);
             // 
             // colorPropertiesToolStripMenuItem
             // 
-            this.colorPropertiesToolStripMenuItem.Enabled = false;
             this.colorPropertiesToolStripMenuItem.Name = "colorPropertiesToolStripMenuItem";
             this.colorPropertiesToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
             this.colorPropertiesToolStripMenuItem.Text = "Color Details";
+            this.colorPropertiesToolStripMenuItem.Click += new System.EventHandler(this.colorPropertiesToolStripMenuItem_Click);
             // 
             // pictureBox1
             // 
@@ -107,16 +126,166 @@
             this.pictureBox1.Size = new System.Drawing.Size(256, 256);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(256, 24);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 356);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.trackBar2);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.trackBar3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(0, 280);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(256, 100);
+            this.panel2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "G";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "R";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(111, 73);
+            this.textBox4.MaxLength = 3;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(27, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "$00";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 44);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "$00";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 15);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "$00";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 3);
+            this.textBox1.MaxLength = 5;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "$0000";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(135, 64);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar2.Maximum = 31;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(120, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(135, 35);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar1.Maximum = 31;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(120, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(135, 6);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar3.Maximum = 31;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(120, 45);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Color $00";
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(0, 22);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(94, 78);
+            this.panel3.TabIndex = 0;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
             // 
             // CGRAMEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 280);
+            this.ClientSize = new System.Drawing.Size(656, 380);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.MaximumSize = new System.Drawing.Size(272, 319);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(672, 419);
+            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(272, 319);
             this.Name = "CGRAMEditor";
             this.ShowInTaskbar = false;
@@ -126,6 +295,11 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,5 +315,20 @@
         private System.Windows.Forms.ToolStripMenuItem importDataToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exportDataToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
--- SuperTileMapper/SuperTileMapper/CGRAMEditor.cs
+++ SuperTileMapper/SuperTileMapper/CGRAMEditor.cs
@@ -12,10 +12,14 @@ namespace SuperTileMapper
 {
     public partial class CGRAMEditor : Form
     {
+        bool showHexEditor = false;
+        int showDetails = -1;
+
         public CGRAMEditor()
         {
             InitializeComponent();
             Redraw();
+            ResizeMe();
         }
 
         private void importDataToolStripMenuItem_Click(object sender, EventArgs e)
@@ -30,6 +34,35 @@ namespace SuperTileMapper
             ExportData export = new ExportData("CGRAM", Data.CGRAM);
             DialogResult result = export.ShowDialog();
             if (result == DialogResult.OK) Redraw();
+        }
+
+        private void SetSize(Size s)
+        {
+            this.MaximumSize = s;
+            this.MinimumSize = s;
+            this.Size = s;
+        }
+        public void ResizeMe()
+        {
+            hexEditorToolStripMenuItem.Checked = showHexEditor;
+            colorPropertiesToolStripMenuItem.Checked = (showDetails>=0);
+            if (showHexEditor && (showDetails >= 0))
+            {
+                SetSize(new Size(672, 419));
+                panel1.Height = 356;
+            } else if (showHexEditor)
+            {
+                SetSize(new Size(672, 319));
+                panel1.Height = 256;
+            } else if ((showDetails >= 0))
+            {
+                SetSize(new Size(272, 419));
+            } else
+            {
+                SetSize(new Size(272, 319));
+            }
+            panel1.Visible = showHexEditor;
+            panel2.Visible = (showDetails >= 0);
         }
 
         public void Redraw()
@@ -51,9 +84,67 @@ namespace SuperTileMapper
             pictureBox1.Image = img;
         }
 
+        private void UpdateDetails()
+        {
+            label1.Text = "Color $" + Util.DecToHex(showDetails, 2);
+            int val = (Data.CGRAM[2 * showDetails] | (Data.CGRAM[2 * showDetails + 1] << 8));
+            textBox1.Text = "$" + Util.DecToHex(val, 4);
+            textBox2.Text = "$" + Util.DecToHex(val & 0x001F, 2);
+            textBox3.Text = "$" + Util.DecToHex((val & 0x03E0) >> 5, 2);
+            textBox4.Text = "$" + Util.DecToHex((val & 0x7C00) >> 10, 2);
+            trackBar3.Value = val & 0x001F;
+            trackBar1.Value = (val & 0x03E0) >> 5;
+            trackBar2.Value = (val & 0x7C00) >> 10;
+            panel3.BackColor = Data.GetCGRAMColor(showDetails);
+        }
+
         private void CGRAMEditor_Load(object sender, EventArgs e)
         {
+        }
 
+        private void hexEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showHexEditor = !showHexEditor;
+            ResizeMe();
+        }
+
+        private void colorPropertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showDetails = showDetails >= 0 ? -1 : 0;
+            if (showDetails >= 0)
+            {
+                UpdateDetails();
+            }
+            ResizeMe();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (showDetails >= 0)
+            {
+                showDetails = 0x10 * (e.Y / 16) + (e.X / 16);
+                UpdateDetails();
+            }
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = Data.GetCGRAMColor(showDetails);
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Color c = colorDialog1.Color;
+                int cg = ((c.B & 0xF8) << 7) | ((c.G & 0xF8) << 2) | ((c.R & 0xF8) >> 3);
+                Data.CGRAM[2 * showDetails] = (byte)(cg & 0x00FF);
+                Data.CGRAM[2 * showDetails + 1] = (byte)((cg & 0x7F00) >> 8);
+                UpdateDetails();
+                Redraw();
+            }
         }
     }
 }
